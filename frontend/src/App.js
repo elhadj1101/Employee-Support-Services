948b24857961f120508b0417aae1f4096f7beac2
Ilyeshaddad337 <i.haddad@esi-sba.dz>
2024-03-04 15:37:42 +0100

    Added dashboard navbar and add form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import LoginPage from './Components/loginForm/LoginForm'
+import Dashboard from './pages/Dashboard';
 import  Signup from './pages/Signup'
 function App() {
   return (
     <main className='h-screen w-full'>
+      <Dashboard />
      <Signup />
     </main>
 
