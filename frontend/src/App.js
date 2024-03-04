2287c2b490b1b9d354f22cb3158cff3488670921
Omri Mohammed Ilyes <ilyesomri7@gmail.com>
2024-03-04 20:04:02 +0100

    tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Login from './pages/Login';
 import  Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
 
 import { Route , Routes } from 'react-router-dom';
 function App() {
@@ -9,6 +10,7 @@ function App() {
      <Routes>
          <Route exact path='/' element={<Login />} />
          <Route path='/signup' element={<Signup />} />
+         <Route path='/dashboard' element={<Dashboard />} />
        </Routes>
     </main>
 
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,36 +4,35 @@ import SearchIcon from "../Components/icons/SearchIcon";
 
 function Dashboard() {
   return (
-    <div className="w-full h-screen  bg-lightgray">
+    <div className="w-full h-screen    bg-lightgray">
       <Navbar />
       <div className="px-6 py-2">
         <h1 className="text-3xl text-black font-bold">
           Listes des utilisateurs
         </h1>
         <div className="py-3">
-          <form className="max-w-full">
+          <form className="max-w-full" onSubmit={()=>{console.log('test')}}>
             <label
               htmlFor="default-search"
-              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+              className="mb-2 text-sm font-medium text-gray-900 sr-only "
             >
               Search
             </label>
             <div className="relative">
               <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                <SearchIcon classNames="w-4 h-4 text-gray-500 dark:text-gray-400" />
+                <SearchIcon classNames="w-4 h-4 text-gray-500 " />
               </div>
               <input
                 type="search"
                 id="default-search"
-                className="block w-full p-4 ps-10 text-sm text-gray-900 outline border border-gray-300 rounded-lg bg-gray-50 "
+                className="block w-full p-4 ps-10 text-sm text-gray-900 rounded-lg bg-gray-50 "
                 placeholder="Vous cherchez quel utilisateur ..."
                 required
               />
               <button
                 type="submit"
                 className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm px-4 py-2 "
-              >
-                Recherche
+              >Recherche
               </button>
             </div>
           </form>
