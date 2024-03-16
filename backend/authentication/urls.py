ceace72d420769a4f461696694d90f69b71e2301
kali <kalimail>
2024-03-16 01:35:48 -0400

    commit-url

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CreateUserView, SignupView
+from .views import CreateUserView, SignupView, ListEmployeesView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
