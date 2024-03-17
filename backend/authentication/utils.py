280c661a5e65f81bf3d5164eb674220dfb5a4e0c
Abdouomar22 <amohamedabdelillah@gmail.com>
2024-03-18 00:00:54 +0100

    add max loan function

--- backend/authentication/utils.py
+++ backend/authentication/utils.py
@@ -2,3 +2,13 @@
 
 def is_digits(st):
     return all([c.isdigit() for c in st]) and (st is not None)
+
+def max_loan(salary):
+    return 0.3 * salary * 12
+
+def loan_is_possible(salary,duration,loan):
+    if duration > 12 :
+        return False
+    if loan > 0.3 * duration * salary :
+        return False
+    return True
