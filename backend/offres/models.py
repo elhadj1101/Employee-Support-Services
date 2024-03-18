2973b76b46f4250158fbbadb4b4f3485c61ccc97
Ilyeshaddad337 <i.haddad@esi-sba.dz>
2024-03-18 17:04:37 +0100

    added post_delete call to cleanup files on delete

--- backend/offres/models.py
+++ backend/offres/models.py
@@ -1,9 +1,37 @@
 from django.db import models
 from django.urls import reverse
-from django.utils.text import slugify
-from django.db.models import CheckConstraint, Q, F
-from  time import time
-# Create your models here.
+from django.core.files.storage import default_storage
+from django.db.models.signals import post_delete
+import os
+
+def file_cleanup(sender, **kwargs):
+    """
+    File cleanup callback used to emulate delete file fields when deleting instances.
+    change the field variable to the name of the file field you want to cleanup.
+    behavior using signals. Initially django deleted linked
+    files when an object containing a File/ImageField was deleted.
+
+    Usage:
+    >>> from django.db.models.signals import post_delete
+    >>> post_delete.connect(file_cleanup, sender=MyModel, dispatch_uid="mymodel.file_cleanup")
+    """
+    field = "cover"
+    inst = kwargs["instance"]
+    f = getattr(inst, field)
+    m = inst.__class__._default_manager
+    if (
+        hasattr(f, "path")
+        and os.path.exists(f.path)
+        and not m.filter(
+            **{"%s__exact" % field: getattr(inst, field)}
+            ).exclude(pk=inst._get_pk_val())
+        ):
+        try:
+            default_storage.delete(f.path)
+        except:
+            pass
+
+
 class Offre (models.Model):
     published_at = models.DateField(auto_now_add = True)
     title = models.CharField(max_length = 35)
@@ -25,3 +53,7 @@ class Offre (models.Model):
     
     def get_absolute_url(self):
         return reverse("offre_detail", kwargs={"pk": self.pk})
+    
+post_delete.connect(
+    file_cleanup, sender=Offre, dispatch_uid="offre.file_cleanup"
+)
