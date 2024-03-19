bb361c7fe86174771d9308bd67ac4a70326cf6ef
Omri Mohammed Ilyes <ilyesomri7@gmail.com>
2024-03-19 15:34:18 +0100

    d

--- backend/offres/permissions.py
+++ backend/offres/permissions.py
@@ -3,4 +3,5 @@ from backend.settings import CAN_PUBLISH_OFFRE
 
 class CanPublishOffre(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (request.user and request.user.role in CAN_PUBLISH_OFFRE )
+        perm = (request.user and request.user.is_authenticated) if request.method == 'GET' else (request.user and request.user.role in CAN_PUBLISH_OFFRE )
+        return perm
--- backend/requests/models.py
+++ backend/requests/models.py
@@ -15,42 +15,52 @@ class Loan(models.Model):
         ('ccp','ccp') , 
         ('banque','banque'),
     ]
+
     employee = models.ForeignKey(Employee , on_delete = models.CASCADE   )
+    request_created_at = models.DateField(auto_now_add = True)
     loan_amount = models.CharField(max_length = 10 , null = False)
-    loan_motivation = models.TextField(null = False)
     payment_method = models.TextField(null=False , choices = payment_method_options)
-    loan_status = models.CharField(choices = status_options , max_length = 30  , null = False)
+    loan_motivation = models.TextField(null = False)
     start_loan_date = models.DateField(null = True)
     loan_period = models.CharField(max_length = 2 , null = False)
-
+    loan_status = models.CharField(choices = status_options , max_length = 30  , null = False)
 
 
 #Financial_aid
 class Financial_aid(models.Model):
     financial_aid_type_options = [
-        ('dece','dece') , 
-        ('naissance_bebe','naissance d\'un bébé'),
-        ('mariage','mariage') , 
+        ('family_member_death', 'décès d\'un membre de famille'), 
+        ('employee_death', 'décès d\'un employé'),
+        ('child_birth', 'naissance d\'un fils'),
+        ('mariage', 'mariage'), 
     ]
+    financial_aid_status_options = [
+        ('waiting', 'en attente'),
+        ('refused', 'refusé'),
+        ('approved', 'approuvé')
+    ]
+    family_member_death_options = [
+        ('wife', 'spouse') , 
+        ('son', 'fils'),
+        ('parent' , 'parent'), 
+        ('brother' ,'frère')
+    ]
+
+
     employee = models.ForeignKey(Employee , on_delete = models.CASCADE)
-    request_created_at = models.DateField(auto_now_add = True)
-    financial_aid_amount = models.CharField(max_length = 10)
+    request_created_at = models.DateField(auto_now_add = True )
     financial_aid_type = models.CharField(max_length = 255 , choices = financial_aid_type_options)
-
-
-
+    family_member_death = models.CharField(max_length = 50 , null = True )
+    financial_aid_amount = models.CharField(max_length = 10)
+    financial_aid_status = models.CharField(max_length = 50  , choices = financial_aid_status_options ) 
+    request_response_at = models.DateField()
 
 # Document
 class Document(models.Model):
+
     employee = models.ForeignKey(Employee , on_delete = models.CASCADE )
-    financial_aid = models.ForeignKey(Financial_aid , on_delete = models.CASCADE , null = True )
+    financial_aid = models.ForeignKey(Financial_aid , on_delete = models.CASCADE   )
     document_name = models.CharField(max_length = 255)
-    document_file = models.FileField(upload_to='%Y/%m/%d' )
-
-    
-    
-
-
-
+    document_file = models.FileField(upload_to='docs/%Y/%m/%d' )
    
     
