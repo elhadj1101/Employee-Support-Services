491f3e762fcee4d7cf6dff594942db9093397685
Ilyeshaddad337 <i.haddad@esi-sba.dz>
2024-04-02 17:44:53 +0100

    fixed canViewDetails permission

--- backend/authentication/permissions.py
+++ backend/authentication/permissions.py
@@ -6,4 +6,4 @@ class IsAdmin(permissions.BasePermission):
         return (request.user and request.user.is_superuser)
 class canViewDetails (permissions.BasePermission):
     def has_permission(self, request, view):
-        return (request.user and (request.user.role not in ['employe'])) if (request.method == "GET"  ) else (request.user and request.user.is_superuser)
+        return (request.user and ((request.user.role not in ['employe']) or (request.user and request.user.is_superuser))) if (request.method == "GET"  ) else (request.user and request.user.is_superuser)
