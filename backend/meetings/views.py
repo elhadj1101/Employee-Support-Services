fbad2b94f4b81faf27a2e580d4a9a24c199f94a2
elhadj1101 <faressielhadj@gmail.com>
2024-04-30 01:34:43 +0100

    altering model fields for the Meeting model , create views and api endpoints , managing permissions

--- backend/meetings/views.py
+++ backend/meetings/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from .models import Meeting
+from .serializers import MeetingSerializer
+from .permissions import IsCommitte, CanCreateMeeting
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 
 # Create your views here.
+
+
+class MeetingsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated, IsCommitte, CanCreateMeeting]
+    queryset = Meeting.objects.all()
+    serializer_class = MeetingSerializer
+
+
+class SingleMeetingView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated, CanCreateMeeting]
+    serializer_class = MeetingSerializer
+
+    def get_queryset(self):
+        return Meeting.objects.filter(pk=self.kwargs["pk"])
