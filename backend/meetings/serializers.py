c8d51c080aba635a0d08ee2ccaea20046df9eb00
elhadj1101 <faressielhadj@gmail.com>
2024-04-30 11:54:44 +0100

    add validation for Meeting model fields

--- backend/meetings/serializers.py
+++ backend/meetings/serializers.py
@@ -6,3 +6,11 @@ class MeetingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Meeting
         fields = "__all__"
+
+    def validate(self, attrs):
+        start_time = attrs["start_time"]
+        end_time = attrs["end_time"]
+
+        if start_time > end_time:
+            raise serializers.ValidationError("End date must be after start date.")
+        return super().validate(attrs)
