8403520f6840ac587cd71e7bc0a4bf56a5ff908c
ilyeshaddad337 <i.haddad@esi-sba.dz>
2024-05-01 11:41:04 +0100

    added endpoint for analitics

--- backend/funds_management/models.py
+++ backend/funds_management/models.py
@@ -10,7 +10,7 @@ type_choices = [
 class Record(models.Model):
     type = models.CharField(max_length=50,choices=type_choices)
     created_at = models.DateField(auto_now=True)
-    amount = models.DecimalField(max_digits=20,decimal_places=2)
+    amount = models.DecimalField(max_digits=40,decimal_places=2)
     loan = models.ForeignKey(Loan , on_delete = models.CASCADE , null =True,  related_name='records', blank=True,)
     financial_aid = models.ForeignKey(Financial_aid , on_delete = models.CASCADE, related_name='records' , null = True, blank=True,)
     motif = models.TextField(blank=True, null=True)
--- backend/funds_management/tests.py
+++ backend/funds_management/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Record
+import random
+
 
 # Create your tests here.
+def createDummyRecords ():
+    ll = ['expense','income']
+    financial_lid = [14,15,16,17]
+    loan_ids = [8,9,10]
+    k = ['loan','financial_aid']
+    for i in range(1000):
+        kk = random.choice(k)
+        Record.objects.create(type=random.choice(ll),amount=random.randint(100,10000),loan_id=(None if kk !='loan' else random.choice(loan_ids)),financial_aid_id=(None if kk !='financial_aid' else random.choice(financial_lid)),motif="motif")
+        
+        
+createDummyRecords()
