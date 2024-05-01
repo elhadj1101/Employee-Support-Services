4f4bb1871006e7943160af220f39c43ae47b9918
elhadj1101 <faressielhadj@gmail.com>
2024-05-01 23:05:51 +0100

    added meeting scheduling time conditions

--- backend/meetings/serializers.py
+++ backend/meetings/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import Meeting
+from datetime import datetime, date
+from django.utils.timezone import localtime
 
 
 class MeetingSerializer(serializers.ModelSerializer):
@@ -10,7 +12,17 @@ class MeetingSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
         start_time = attrs["start_time"]
         end_time = attrs["end_time"]
-
         if start_time > end_time:
-            raise serializers.ValidationError("End date must be after start date.")
+            raise serializers.ValidationError("End time must be after start time.")
+        meeting_day = attrs["day"]
+        now = datetime.now().time()
+        day = date.today()
+        print(f"this start/end time {start_time , end_time} and this is now time {now}")
+        if (
+            day == meeting_day and (start_time < now or end_time < now)
+        ) or meeting_day < day:
+            raise serializers.ValidationError(
+                "Meeting should be scheduled after current time."
+            )
+
         return super().validate(attrs)
