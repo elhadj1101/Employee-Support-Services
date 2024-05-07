713f0729d1b088bf97e9e0e80478e7edca28338d
ilyeshaddad337 <i.haddad@esi-sba.dz>
2024-05-07 01:06:39 +0100

    database

--- frontend/src/pages/SingleDemandAid.jsx
+++ frontend/src/pages/SingleDemandAid.jsx
@@ -97,7 +97,6 @@ export default function SingleDemandLoan({ employee }) {
         const u = async () => {
           try {
             const usr = await getUser(requestedAid?.employee.id);
-            const usr = await getUser(requestedAid?.employee.id);
             if (usr) {
               sessionStorage.setItem("requestedAidUser", JSON.stringify(usr));
               setUsr(usr);
