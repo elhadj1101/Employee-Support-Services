cbde738fbe34e5284a21271f70b0474fc41985c7
elhadj1101 <faressielhadj@gmail.com>
2024-05-13 11:02:00 +0100

    modifying login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import LoginForm from "../components/loginForm/LoginForm";
 export default function Login() {
   return (
-    <section className="h-screen w-full  flex items-center  justify-center ">
-      <LoginForm />
+    <section className="h-screen w-full  flex items-center  justify-center bg-gray-100">
+      <div className="w-1/3 h-2/3 flex items-center justify-center bg-white rounded-xl shadow-lg ">
+
+        <LoginForm />
+      </div>
     </section>
   );
 }
